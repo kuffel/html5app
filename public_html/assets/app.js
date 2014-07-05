48041fac1b017bd1a87b516daf09bf9ba33940e6
adam <adam@kuffel.eu>
2014-07-05 15:55:23 +0200

    Created basic project structure.

---
+++ public_html/assets/app.js
@@ -0,0 +1,46 @@
+
+var Application = Backbone.Router.extend({
+    
+    initialize: function(options){
+        Backbone.history.start();
+        console.log('initialize');
+    },
+    
+    routes: {
+        "*actions": "routeDefault"
+    },
+    
+    routeDefault : function( actions ){
+        /*
+        var showPageID = '#app-page-main';
+        if(actions !== null){
+            showPageID = '#app-page-'+actions;
+        }
+        var page = $(showPageID+'.app-page');
+        if(page.length > 0){
+            $('.app-page').hide();
+            $(showPageID+'.app-page').show();
+        }else{
+            $('.app-page').hide();
+            $('#app-page-404.app-page').show();
+        }
+        console.log(showPageID);
+        */
+    }
+    
+    
+    
+    
+    
+    
+});
+
+
+
+$(document).ready(function(){
+    //$('[href="'+document.location.pathname+'"]').parent('li').addClass('active');
+    //console.log(document.location.pathname);
+    //$('.app-page').hide();
+    app = new Application({});       
+});
+
